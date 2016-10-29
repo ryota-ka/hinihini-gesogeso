0b508b79903d76338fa7822f29b2c31eee50af5c
Ryota Kameoka <kameoka.ryota@gmail.com>
2016-10-29 12:23:42 +0900

    Un :curry:

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import Lib
 
 main :: IO ()
-main = fetchLatestWeight >>= maybe (pure ()) (\(weight, date) -> tweet (buildText weight date))
+main = fetchLatestWeight >>= maybe (pure ()) (tweet . uncurry buildText)
